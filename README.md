0c59ef9e21e32b887a1583e6ffd919f3bc485139
J Wayston <177466602+jwayston@users.noreply.github.com>
2026-02-09 02:29:33 +0200

    Added support for multiple links batch processing

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Link expander plugin for The Archive
 
-This plugin takes in a text selection (a link) and expands the link by placing the first H1 header found in the target note before the link.
+This plugin takes in a text selection (a link/list of links separated by newline) and expands the link(s) by placing the first H1 header found in the target note before the link.
 
 The link can be partial (e.g. UID). The plugin uses The Archive's internal search matching algorithm and picks the "best match" if found. If no unambiguous match is available, the link will not be expanded.
 
--- com.idonthavedomain.linkexpander.thearchiveplugin/main.js
+++ com.idonthavedomain.linkexpander.thearchiveplugin/main.js
@@ -7,55 +7,63 @@ Author: JW
 
 "use strict";
 
+const LINEBR = "\n";
 
-const selection = input.text.selected;
+const selection = input.text.selected.normalize("NFC");
 if (!selection)
 {
-	cancel("Please provide a text selection");
+    cancel("Please provide a text selection");
 }
 
 const linkRegex = new RegExp(/\[{0,}(.+?)(\]{1,}|$)/);
-const linkMatch = selection.match(linkRegex);
-
-if (!linkMatch)
-{
-	cancel("General link parsing error");
-}
-
-const link = linkMatch[1];
-if(!link)
-{
-	cancel("Couldn't parse link from the selected text");
-}
-
-console.log(`Trying to expand a link [[${link}]]`);
 
 function extractHeader(noteContent)
 {
-	const contentRegexp = new RegExp("^# (.+)\\n");
-	const contentMatch = noteContent.match(contentRegexp);
-	if (contentMatch)
-	{
-		return contentMatch[1];
-	}
-	else
-	{
-		return "No H1 headers found";
-	}
+    const contentRegexp = new RegExp("^# (.+)\\n");
+    const contentMatch = noteContent.match(contentRegexp);
+    if (contentMatch)
+    {
+        return contentMatch[1];
+    }
+    else
+    {
+        return "No H1 headers found";
+    }
 }
 
 function findNote(linkText)
 {
-	const fileMatch = app.search(linkText)
+    const fileMatch = app.search(linkText)
 
-	if (fileMatch.bestMatch)
-	{
-		const expanded = `${extractHeader(fileMatch.bestMatch.content)} ${selection}`;
-		return expanded;
-	}
+    if (fileMatch.bestMatch)
+    {
+        const expanded = `${extractHeader(fileMatch.bestMatch.content)} [[${linkText}]]`;
+        return expanded;
+    }
 
-	return `<Error: no unambiguous note found> ${selection}`;
+    return `<Error: no unambiguous note found> ${selection}`;
 }
 
-const expandedLinkText = findNote(link);
-output.insert.text = expandedLinkText;
+
+let outputText = "";
+
+selection.split(LINEBR).forEach(elem =>
+    {
+        const linkMatch = elem.match(linkRegex);
+
+        if (!linkMatch)
+        {
+            cancel("General link parsing error");
+        }
+
+        const link = linkMatch[1];
+        if(!link)
+        {
+            cancel("Couldn't parse link from the selected text");
+        }
+
+        const expandedLinkText = findNote(link);
+        outputText += `${expandedLinkText}\n`;
+    })
+
+output.insert.text = outputText;
